4a282af41ef355bd8c5da177d478ea040fd477a7
alvin <alvinecom2018@gmail.com>
2025-02-05 08:49:04 +0700

    --

--- src/screens/quran/Surah.tsx
+++ src/screens/quran/Surah.tsx
@@ -5,6 +5,7 @@ import {
   RefreshControl,
   TouchableOpacity,
   TextInput,
+  ActivityIndicator,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
 import BackButton from '../../components/BackButton';
@@ -15,14 +16,17 @@ import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 
 export default function Surah({navigation}: any) {
   const [surahs, setSurahs] = useState<ISurah[]>([]);
-  const [search, setSearch] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
 
   const getSurahs = async () => {
+    setLoading(true);
     try {
       const result = await axios.get('https://equran.id/api/v2/surat');
       setSurahs(result.data.data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
@@ -52,11 +56,15 @@ export default function Surah({navigation}: any) {
           placeholder="Cari disini..."
           placeholderTextColor={'gray'}
           style={{color: 'black', width: '100%'}}
-          onChangeText={(e) => {
+          onChangeText={e => {
             if (e === '') {
               return getSurahs();
             }
-            setSurahs(surahs.filter((s) => s.namaLatin.toLowerCase().includes(e.toLowerCase())))
+            setSurahs(
+              surahs.filter(s =>
+                s.namaLatin.toLowerCase().includes(e.toLowerCase()),
+              ),
+            );
           }}
         />
       </View>
@@ -65,24 +73,30 @@ export default function Surah({navigation}: any) {
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
         contentContainerStyle={{marginTop: 20, paddingBottom: 150}}>
-        {surahs.map((surah: ISurah, index: number) => (
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('ListAyah', {surahId: surah.nomor});
-            }}
-            key={index}
-            style={{
-              marginVertical: 5,
-              borderBottomWidth: 1,
-              borderRadius: 10,
-              padding: 10,
-            }}>
-            <Text style={{fontSize: 20}}>
-              {surah.nomor}. {surah.namaLatin} ({surah.tempatTurun}){' '}
-              {surah.jumlahAyat} Ayat
-            </Text>
-          </TouchableOpacity>
-        ))}
+        {loading ? (
+          <ActivityIndicator size={'large'} color={'green'} />
+        ) : (
+          <>
+            {surahs.map((surah: ISurah, index: number) => (
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('ListAyah', {surahId: surah.nomor});
+                }}
+                key={index}
+                style={{
+                  marginVertical: 5,
+                  borderBottomWidth: 1,
+                  borderRadius: 10,
+                  padding: 10,
+                }}>
+                <Text style={{fontSize: 20}}>
+                  {surah.nomor}. {surah.namaLatin} ({surah.tempatTurun}){' '}
+                  {surah.jumlahAyat} Ayat
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </>
+        )}
       </ScrollView>
     </View>
   );
